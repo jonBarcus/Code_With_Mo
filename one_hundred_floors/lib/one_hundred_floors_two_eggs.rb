5e6929e691c2080c73c67ba63fbfa27957b488bb
jonBarcus <barcus.j@gmail.com>
2015-02-02 14:16:09 -0500

    adjusted code...sorry for crappy commits

--- one_hundred_floors/lib/one_hundred_floors_two_eggs.rb
+++ one_hundred_floors/lib/one_hundred_floors_two_eggs.rb
@@ -1,4 +1,29 @@
-def egg_test(floors)
+class Building
+
+  def initialize(floors, terminal_floor)
+    @floors = floors
+    @terminal_floor = terminal_floor
+  end
+
+  def floors
+    return @floors
+  end
+
+  def terminal_floor
+    return @terminal_floor
+  end
+
+  def starting_level
+
+    starting_level = @floors / 2
+
+    return starting_level
+
+  end
+
+  def egg_test
+
+  end
 
 
 end
