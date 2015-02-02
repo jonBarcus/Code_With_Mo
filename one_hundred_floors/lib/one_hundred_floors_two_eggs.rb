3cb8f09be46fca779ba31ace6f32fdc6bfabba82
jonBarcus <barcus.j@gmail.com>
2015-02-02 18:34:17 -0500

    git mistake...latest update here

--- one_hundred_floors/lib/one_hundred_floors_two_eggs.rb
+++ one_hundred_floors/lib/one_hundred_floors_two_eggs.rb
@@ -6,7 +6,7 @@ class Building
     @floors = floors
     @terminal_floor = terminal_floor
     @highest_safe_floor = 0
-    @test = false
+    @counter = 1
   end
 
   def floors
@@ -30,12 +30,25 @@ class Building
     return new_start
   end
 
+  def move_test_up(old_starting_level)
+    if old_starting_level < starting_level
+      new_start = (starting_level / 2 )
+    else
+      new_start = ((floors - old_starting_level) / 2 ) + old_starting_level
+    end
+    return new_start
+  end
+
   def egg_break(floor_drop)
     if floor_drop == terminal_floor
       @highest_safe_floor = floor_drop - 1
       return true
     elsif floor_drop > terminal_floor
       @new_level = move_test_down
+      @broken_egg = true
+      return @new_level && false
+    elsif floor_drop < terminal_floor && @broken_egg == false
+      @new_level = move_test_up(floor_drop)
       return @new_level && false
     else
       @highest_safe_floor = floor_drop
@@ -44,26 +57,21 @@ class Building
     end
   end
 
-
-
-  # def egg_break(floor_drop)
-  #   if floor_drop >= terminal_floor
-  #     return true
-  #   else
-  #     return floor_drop && false
-  #   end
-  # end
-
-
   def egg_test
 
+    @broken_egg = false
     @new_level = starting_level
 
     while egg_break(@new_level) == false
+      @counter += 1
     end
 
     return @highest_safe_floor
 
+  end
+
+  def counter_method
+      return @counter
   end
 
 
